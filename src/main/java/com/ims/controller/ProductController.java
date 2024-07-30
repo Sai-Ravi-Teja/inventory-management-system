0dc35d54eda3c06c0a1fd92ac08e57c1a1a8c155
Sai Ravi Teja Baswa <raviteja5797@gmail.com>
2024-07-31 02:31:26 +0530

    Added Swagger 3.0 and IMS Banner

--- src/main/java/com/ims/controller/ProductController.java
+++ src/main/java/com/ims/controller/ProductController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ims.dto.ProductRequest;
 import com.ims.service.ProductService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,12 +24,15 @@ public class ProductController {
 
 	private final ProductService productService;
 
+	@Operation(summary = "Create and Save a Product")
+	@ApiResponses(value = { @ApiResponse(responseCode = "201", description = "Created and Saved a Product", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = ProductRequest.class)) }) })
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public String createProduct(@RequestBody ProductRequest productRequest) {
 
 		productService.createProduct(productRequest);
-		return "Order Placed Successfully";
+		return "Product added to DB Successfully";
 	}
 
 }
